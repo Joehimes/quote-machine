a577bdfaf329dc1abfe3a4db5e87dc335c5b3e78
Joe Himes <joehimes42@gmail.com>
2020-03-27 19:18:53 -0400

    Updates

---
+++ precache-manifest.52a5a43772d66c0df58db08537950c02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c848fd0a5380feb84e01edceed1866b2",
+    "url": "/quote-machine/index.html"
+  },
+  {
+    "revision": "985038cae1438b4e10f7",
+    "url": "/quote-machine/static/css/main.cd17c672.chunk.css"
+  },
+  {
+    "revision": "0a86be2e57cb6620c1a1",
+    "url": "/quote-machine/static/js/2.7520ed6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quote-machine/static/js/2.7520ed6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "985038cae1438b4e10f7",
+    "url": "/quote-machine/static/js/main.0aa1805d.chunk.js"
+  },
+  {
+    "revision": "aae0ce78e2995d5dba4d",
+    "url": "/quote-machine/static/js/runtime-main.e5ed0aff.js"
+  }
+]);
