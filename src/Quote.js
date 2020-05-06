38c51aae29324e02a8849197e00540b60c4b99c4
Joe Himes <joehimes42@gmail.com>
2020-05-06 10:58:20 -0400

    Updated colors

--- src/Quote.js
+++ src/Quote.js
@@ -32,18 +32,22 @@ class Quote extends React.Component {
         const twitterURL = 'https://twitter.com/intent/tweet?hashtags=quotes&text=' + encodeURIComponent('"' + this.state.quote + '" ' + this.state.author)
 
         return (
-            <div className="quote-box" id="quote-box">
-                <p className="text" id="text">{this.state.quote}</p>
-                <p className="author" id="author">- {this.state.author} -</p>
-                <a
-                    className="twitter-share-button twitter"
-                    id="tweet-quote"
-                    href={twitterURL}
-                    target="_blank"
-                >
-                    Tweet
-                </a>
-                <button className="new-quote" id="new-quote" onClick={this.getNewQuote}>New Quote</button>
+            <div className="quote-box">
+                <div>
+                    <p className="text">{' '}{this.state.quote}{' '}</p>
+                    <p className="author">{this.state.author}</p>
+                </div>
+                <div>
+                    <a
+                        className="twitter-share-button twitter"
+                        id="tweet-quote"
+                        href={twitterURL}
+                        target="_blank"
+                    >
+                        Tweet
+                    </a>
+                    <button className="new-quote" variant="primary" onClick={this.getNewQuote}>New Quote</button>
+                </div>
             </div>
         )
     }
